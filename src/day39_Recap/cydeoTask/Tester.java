661acbde7035e1df46c4a13d692f826b9254eb30
anast <anastasiyagok81@hotmail.com>
2022-01-18 19:42:14 -0800

    day39: Recap

--- src/day39_Recap/cydeoTask/Tester.java
+++ src/day39_Recap/cydeoTask/Tester.java
@@ -1,9 +1,21 @@
 package day39_Recap.cydeoTask;
 
-public class Tester {
+public class Tester extends Employee{
+
+    public Tester(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+
+    public void createTicket(){
+        System.out.println(getJobTitle()+" "+getName()+" is creating a ticket");
+    }
+
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is testing applications");
+    }
 }
 /*
-  3. Create a su class of Employee named Tester
+  3. Create a subclass of Employee named Tester
 
             Override the work method
 
