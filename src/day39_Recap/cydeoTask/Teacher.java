6d41461340b3d2b9f509e5996875eb01660d429f
anast <anastasiyagok81@hotmail.com>
2022-01-18 19:51:36 -0800

    day39: Recap

--- src/day39_Recap/cydeoTask/Teacher.java
+++ src/day39_Recap/cydeoTask/Teacher.java
@@ -1,9 +1,17 @@
 package day39_Recap.cydeoTask;
 
-public class Teacher {
+public class Teacher extends Employee {
+
+    public Teacher(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is teaching");
+    }
 }
 /*
-  5. Create a sub class of Employee named Teacher
+  5. Create a subclass of Employee named Teacher
 
             Override the work method
 
