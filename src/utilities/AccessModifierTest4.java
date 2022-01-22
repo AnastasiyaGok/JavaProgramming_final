e4d58a5ee7a5f7db19d04b447f343bf5ff63ba47
anast <anastasiyagok81@hotmail.com>
2022-01-21 21:42:12 -0800

    day40: Final Keyword

--- src/utilities/AccessModifierTest4.java
+++ src/utilities/AccessModifierTest4.java
@@ -1,8 +1,15 @@
 package utilities;
 
-public class AccessModifierTest4 {
+import day40_FinalKeyword.ProtectedAccessModifier;
+
+public class AccessModifierTest4 extends ProtectedAccessModifier {
     public static void main(String[] args) {
 
+     //   System.out.println(AccessModifierTest4.name1);// default is not visible outside the package
+        System.out.println(AccessModifierTest4.name2);// protected is visible outside package in subclass only
+
+     //   AccessModifierTest4.method1();
+        AccessModifierTest4.method2();
 
     }
 }
